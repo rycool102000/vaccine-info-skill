e25815c7160dcc816539dc7649ef1d45804e9e96
Ryan Miller <83560763+rycool102000@users.noreply.github.com>
2021-05-11 16:47:53 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+import re
+import sys
+import json
+from os.path import dirname, join
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler, intent_file_handler
+from mycroft.messagebus.message import Message
+from mycroft.skills.core import resting_screen_handler
 
 class VaccineInfo(MycroftSkill):
     def __init__(self):
@@ -13,6 +19,13 @@ class VaccineInfo(MycroftSkill):
     @intent_file_handler('info.harvey.intent')
     def handle_info_harvey(self, message):
         self.speak_dialog('info.harvey')
+
+    @intent_file_handler('info.bus.intent')
+    def handle_info_harvey(self, message):
+        self.gui.clear()
+        self.enclosure.display_manager.remove_active()
+        self.gui.show_image("qr.png", "Scan here to find bus schedules", "Scan here to find bus schedules")
+        
         
 def create_skill():
     return VaccineInfo()
